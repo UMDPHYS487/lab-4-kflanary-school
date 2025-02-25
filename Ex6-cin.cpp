fbbe21a8a628df8d5e87a07c1408c7ef14055bea
Kevin Flanary <kflanary@terpmail.umd.edu>
2025-02-25 20:09:44 +0000

    pointer

--- Ex6-cin.cpp
+++ Ex6-cin.cpp
@@ -1,1 +1,22 @@
-// your code here
+// add your code here
+#include <iostream>
+using namespace std;
+
+int y;
+int *x;
+
+int main(){
+    y = 10;
+    x = &y;
+    cout << "Value of y is: " << y << '\n';
+    cout <<"Its pointer: " << x <<'\n';
+    cout <<"Value at that pointer: " << *x << '\n';
+
+    cout << "Give y a new value: ";
+    cin >> y;
+
+    cout << "The new value of y is: " << y << '\n';
+    cout <<"Its pointer: " << x <<'\n';
+    cout <<"Value at that pointer: " << *x << '\n';    
+    return 0;
+}
