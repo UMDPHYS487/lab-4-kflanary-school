484b90c4707f9ed836ab433e2c796346766ffd29
Kevin Flanary <kflanary@terpmail.umd.edu>
2025-02-25 21:38:48 +0000

    parts 7 and 9

--- Ex7-SwitchCase.cpp
+++ Ex7-SwitchCase.cpp
@@ -3,19 +3,32 @@
 using namespace std;
 
 int main(){
-    int N;
+    int N, Truth;
+    bool truth1, truth2;
+    float k;
+
     cout << "Give me a number: ";
     cin >> N;
 
-    if(N<10 && N%2 == 0){
-        cout << "Your number is " << N << " and its square is " << N*N << "\n";
-    }
-    else if (N%2 == 1){
-        float k = N*(.5);
-        cout << "Your number is " << N << " and its half is " << k << "\n";
-    }
-    else{
-        cout << "hahaha\n";
+    truth1 = N%2 == 1 || -N%2 == 1; truth2 = N < 10;
+    Truth = 20*truth1 + truth2;
+
+    switch(Truth){
+        case 20:
+            k = N*(.5);
+            cout << "Your number is " << N << " and its half is " << k << "\n";
+            break;
+        case 21:
+            k = N*(.5);
+            cout << "Your number is " << N << " and its half is " << k << "\n";
+            break; 
+        case 1:
+            k = N*N;
+            cout << "Your number is " << N << " and its square is " << k << "\n";
+            break;
+        case 0:
+            cout<<"hahaha\n";
+            break;
     }
     return 0;
 }
