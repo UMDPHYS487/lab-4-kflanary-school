af668fde01e0064cf29d1eeb3d33c6854d9720f1
Kevin Flanary <kflanary@terpmail.umd.edu>
2025-02-25 19:52:32 +0000

    factorials

--- Factorial-ex3.cpp
+++ Factorial-ex3.cpp
@@ -1,1 +1,14 @@
-// Add your code here
+// add your code here
+#include <iostream>
+using namespace std;
+
+int main(){
+    for (int n=1; n<=10; n++){
+        int fact = 1;
+        for (int i = 1; i<=n; i++){
+            fact = fact*i;
+        }
+        cout << "The factorial of " << n << " is " << fact << '\n';
+    }
+    return 0;
+}
--- FactorialLoop-ex4.cpp
+++ FactorialLoop-ex4.cpp
@@ -1,1 +1,18 @@
-// Add your code here
+// add your code here
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+    int fact = 1;
+    for (int i = 1; i<=n;i++){
+        fact = fact*i;
+    }
+    return fact;
+}
+
+int main(){
+    for (int i=1; i<=10; i++){
+        cout << "The factorial of " << i << " is " << factorial(i) << '\n';
+    }
+    return 0;
+}
