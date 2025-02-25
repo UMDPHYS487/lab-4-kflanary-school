19f280c024427474baa5fcbb7d70aa8d219c190b
Kevin Flanary <kflanary@terpmail.umd.edu>
2025-02-25 20:40:52 +0000

    bitset

--- Ex8-Bitwise.cpp
+++ Ex8-Bitwise.cpp
@@ -1,1 +1,21 @@
-// Your code here
+// add your code here
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main(){
+    bitset<8> b{0b01110010};
+        cout << b << " (initial value)\n";
+
+        for (int i = 0; i < 8; i++){
+            if(b.test(i) == true){
+                b.reset(i);
+            }
+            else{
+                b.set(i);
+            }
+            cout << b << '\n';
+        }
+        cout << b << " (final value) \n";
+    return 0;
+}
