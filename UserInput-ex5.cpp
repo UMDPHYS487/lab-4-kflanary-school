e27154403cab4a2541ac81c665e10b485876ea86
Kevin Flanary <kflanary@terpmail.umd.edu>
2025-02-25 19:52:45 +0000

    user_factorials

--- UserInput-ex5.cpp
+++ UserInput-ex5.cpp
@@ -1,1 +1,23 @@
-// Add your code here
+// add your code here
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+    int fact = 1;
+    for (int i = 1; i<=n;i++){
+        fact = fact*i;
+    }
+    return fact;
+}
+
+int main(){
+    int N = -1;
+    while (N<0){
+        cout << "Please enter a nonnegative integer value: ";
+        cin >> N;
+        if (N >= 0) {
+            cout << "The value you entered is " << N << " and its factorial is " << factorial(N) << '\n';
+        }
+    }
+    return 0;
+}
